e67e7a0674bcf542fcda5882e09181b1cb703f56
Marcus Voltolim <marcus.voltolim@gmail.com>
2022-05-03 19:23:05 -0300

    Initial commit, creating gradle project

---
+++ src/main/groovy/io/marcusvoltolim/dynamodbenhanced/DynamodbEnhancedApplication.groovy
@@ -0,0 +1,13 @@
+package io.marcusvoltolim.dynamodbenhanced
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class DynamodbEnhancedApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(DynamodbEnhancedApplication, args)
+    }
+
+}
---
+++ src/test/groovy/io/marcusvoltolim/dynamodbenhanced/DynamodbEnhancedApplicationTests.groovy
@@ -0,0 +1,13 @@
+package io.marcusvoltolim.dynamodbenhanced
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DynamodbEnhancedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
